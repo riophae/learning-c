320523fd15ef02519549f97b30d6974f22c818e5
Riophae Lee <riophaelee@gmail.com>
2015-07-06 22:00:45 +0800

    add 005

---
+++ 005-fahr-vs-celsius-with-title.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  float fahr, celcius;
+  float lower, upper, step;
+
+  lower = 0;
+  upper = 200;
+  step = 20;
+
+  fahr = lower;
+
+  printf("  F      C\n");
+
+  while (fahr <= upper) {
+    celcius = (5.0/9.0) * (fahr-32);
+    printf("%3.0f %6.1f\n", fahr, celcius);
+    fahr = fahr + step;
+  }
+}
