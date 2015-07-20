e64ee9fc399ef43141e18545b81dd967e3d79f12
Riophae Lee <riophaelee@gmail.com>
2015-07-20 08:02:14 +0800

    add 023 & 024

---
+++ 023-power.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int power(int base, int n)
+{
+  int result = 1, i;
+
+  for (i = 0; i < n; i++) {
+    result = result * base;
+  }
+
+  return result;
+}
+
+int main()
+{
+  int n;
+
+  for (n = 0; n <= 10; n++) {
+    printf("n = %d 2^n = %d (-3)^n = %d\n", n, power(2, n), power(-3, n));
+  }
+}
