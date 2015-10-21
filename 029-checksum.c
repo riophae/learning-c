13e9520c9c67466c41d4083c1be32cb563f1ab07
Riophae Lee <riophaelee@gmail.com>
2015-10-21 17:59:34 +0800

    update

--- 029-checksum.c
+++ 029-checksum.c
@@ -5,13 +5,19 @@ int main(void) {
   signed char checksum = -1;
   char c;
 
-  printf("Input any characters then press ENTER to calculate checksum.\n");
+  printf("Input any characters then press ENTER to calculate checksum: \n");
 
-  while ((c = getchar()) != '\n') {
+  c = getchar();
+  printf("Original input: ");
+
+  while (c != '\n') {
     checksum += c;
+    putchar(c);
+    c = getchar();
   }
+  putchar('\n');
 
-  printf("checksum: %d", checksum);
+  printf("checksum: %d\n", checksum);
 
   return EXIT_SUCCESS;
 }
